f6852d4579866a6e64c5efc7ab53e8eb8a6a7303
ThEndGuy <livm04@hotmail.com>
2026-06-29 23:07:06 +0100

    Updated reloader to use new file structure

--- reloader.py
+++ reloader.py
@@ -4,9 +4,9 @@ from livereload import Server
 server = Server()
 
 # Watch specific files and trigger a reload when they change
-server.watch("./index.html")
-server.watch("./style.css")
-server.watch("./game.js")
+server.watch("./public/index.html")
+server.watch("./public/style.css")
+server.watch("./public/game.js")
 
 # Start serving on port 8000
 server.serve(port=8000, host="127.0.0.1")
